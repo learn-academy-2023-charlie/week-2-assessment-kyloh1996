9433664cb39c9203661ca1bc7727e61a009512f3
kyloh1996 <kyloh1996@gmail.com>
2023-04-30 01:20:33 -0500

    saving in the unlikely chance my computer crashes and i lose everything

--- coding-concepts.js
+++ coding-concepts.js
@@ -8,9 +8,9 @@
 // --------------------1) What will this log?
 
 const cohort = "Charlie 2023"
-// console.log(cohort.split(""))
+console.log(cohort.split(""))
 
-// a) Your answer:
+// a) Your answer: I predict the output will be: ["C","h","a","r","l","i","e","","2","0","2","3"] because of the method .split()
 // b) Verify and explain:
 
 // --------------------2) What will this log?
@@ -18,26 +18,26 @@ const cohort = "Charlie 2023"
 const greeter = (name) => {
   `Hello, ${name}!`
 }
-// console.log(greeter("LEARN Student"))
+console.log(greeter("LEARN Student"))
 
-// a) Your answer:
+// a) Your answer: I dont think it will log anything. because name isnt defined...
 // b) Verify and explain:
 
 // --------------------3) What will this log?
 
 const multipliedByTwo = [4, 5, 6, 7, 8].map((number) => number * 2)
-// console.log(multipliedByTwo)
+console.log(multipliedByTwo)
 
-// a) Your answer:
+// a) Your answer:[8,10,12,14,16], because the function maps out the array and multiplies each index by 2
 // b) Verify and explain:
 
 // --------------------4) What will this log?
 
 const onlyOdds = [11, 12, 13, 14, 15].filter((number) => number % 2 !== 0)
-// console.log(onlyOdds)
+console.log(onlyOdds)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: it wont long the odds because the finding out would be stricly equal to(===). i think it will either leave the even number or come back with an error. Then again I dont really understand what the !(not)==(loosly equal) means
+// b) Verify and explain: //OUTPUT: [ 8, 10, 12, 14, 16 ] Seperated the even and odd values [ 11, 13, 15 ] // WHY?
 
 // --------------------5) What will this log?
 
@@ -47,9 +47,9 @@ const myCodingSkills = {
   databases: "PostgreSQL",
   versionControl: "GitHub"
 }
-// console.log(myCodingSkills.languages[0])
+console.log(myCodingSkills.languages[0])
 
-// a) Your answer:
+// a) Your answer: This is an object. myCodingSkills.languages[0] -> myCodingSkills references the _______, and .languages[0] -> references the key languages and the index of 0. With that I think the output will be "javascript".
 // b) Verify and explain:
 
 // --------------------STRETCH: What will this log?
@@ -62,7 +62,7 @@ class Learn {
   }
 }
 const learnStudent = new Learn("George")
-// console.log(learnStudent)
+console.log(learnStudent)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: Learn is the class. name would be "George"; this would be filled in when console logged and the output would be: "George", "charlie" , 2023 .
+// b) Verify and explain: Learn { student: 'George', cohort: 'Charlie', year: 2023 } . 
--- interview-questions.md
+++ interview-questions.md
@@ -9,37 +9,37 @@ Then, research the question to expand on your answer. Even if you feel you have 
 1. What is the difference between a parameter and an argument?
 
 Your answer:
-
+Parameter are for variable declarations. Arguments are actual values passed to a method.
 Researched answer:
 
 2. The JavaScript built in method .map() takes predefined parameters. What are they? Which are required and which are optional?
 
 Your answer:
-
+.map() `iterates through each index of an array. I know one requirment is  the array has to be called upon inorder for .nap() to work. it need to know what its iterating through`
 Researched answer:
 
 3. What is the difference between map and filter?
 
 Your answer:
-
+.map() it for locating where a value is.filter() requires a conditional statement inorder to know what to filter from the given array. 
 Researched answer:
 
 1. What is iteration?
 
 Your answer:
-
+moving along a given set of values from left to right. This process continues until a condition is met.
 Researched answer:
 
 1. What is the difference between a function and a method?
 
 Your answer:
-
+function is the process and method is how that process is conducted. 
 Researched answer:
 
 6. STRETCH: What is hoisting in JavaScript?
 
 Your answer:
-
+well hoisting means to lift up, so im assuming hoisting in javascript is when a code block is pushed to the top. how that happens or what it looks like is beyond me. 
 Researched answer:
 
 ## Looking Ahead: Terms for Next Week
